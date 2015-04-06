1c17a30fcdc31e43dfc3fbb808c56c015a43865a
sabaresabar <sabar_itt@live.com>
2015-04-07 07:48:02 +0800

    TP8_RPL
    Github

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Sabar
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello, Saya Sabar Wicaksana . Nim Saya 6306101005 . saya sangat menyukai praktikum RPL Modul 8 ini; ");
+    }
+}
